f53624f51cce9429e595ce06e21eaa5838fa151d
lazi19 <koaichetassia@yahoo.fr>
2021-05-20 15:40:31 +0200

    débogage bouton supprimer

--- js/panier.js
+++ js/panier.js
@@ -1,10 +1,49 @@
-//récupération des données du local storage 
+//récupération des données des produits du local storage 
 
 let produitEnregistreDansLocalStorage = JSON.parse(localStorage.getItem("basket"));
 console.log('produitEnregistreDansLocalStorage')
 console.log(produitEnregistreDansLocalStorage)
 
 
+// fonction pour la Gestion du boutton supprimer l'article 
+
+const removeElementFromBasket = (id, color, name, quantity) => {
+     console.log("element supprimer");
+    console.log(id, color, name, quantity); 
+        
+    const basketWithoutRemovedElement = produitEnregistreDansLocalStorage.filter(
+      (elem) => {
+        console.log("elem.id !== id && elem.color !== color");
+        console.log(elem.id === id && elem.color === color);
+        console.log("elem.quantity avant le  if");
+        console.log(elem.quantity, elem.id, elem.color );
+           
+
+        if (elem.quantity > 1  ){
+            console.log("le 1er if");
+            if( elem.id === id && elem.color === color){
+                console.log("le 2eme if");
+                elem.quantity = elem.quantity - 1; 
+                return elem.quantity;               
+            }            
+               
+        }else if(elem.id !== id){
+            console.log("le else");
+            return (elem.id !== id );
+        }
+        
+         return (elem.id !== id && elem.color !== color && elem.quantity );
+        
+    }
+    );
+    console.log("basketWithoutRemovedElement");
+    console.log(basketWithoutRemovedElement);
+    localStorage.setItem("basket", JSON.stringify(basketWithoutRemovedElement));
+    console.log(basketWithoutRemovedElement);
+     document.location.reload();
+  };
+  
+// Fin fonction pour la Gestion du boutton supprimer l'article 
 
 // injection de l HTML
 
@@ -26,12 +65,30 @@ if (produitEnregistreDansLocalStorage){
                 <p id="quantite">Quantité : ${element.quantity} </p>
                 <p id="couleur">Couleur : ${element.color} </p>
                 <p id="prix">Prix : ${element.price/100} €</p>
-                <button id="${element.id} " class="btn-supprimer">supprimer ${element.name} ${element.color}</button>            
+                           
             </div>            
          `;
-         
+
+         const deleteButton = document.createElement("button");
+         deleteButton.innerHTML = "supprimer";
+
          panierVide.appendChild(DivProduit); 
-           
+         panierVide.appendChild(deleteButton); 
+
+          // Supression de l'article du local storage
+          console.log("element.id, element.color");
+          console.log(element.id, element.color,element.name, element.quantity);
+
+          deleteButton.addEventListener("click", () =>{
+              console.log("apres le click");
+            //   console.log(removeElementFromBasket(element.id, element.color, element.quantity));
+            removeElementFromBasket(element.id, element.color, element.name, element.quantity);
+            console.log("element.id, element.color, element.quantity");
+            console.log(element.id, element.color, element.name, element.quantity);
+          }        
+         ); 
+
+        
     });
 
 
@@ -40,165 +97,96 @@ if (produitEnregistreDansLocalStorage){
         panierVide.appendChild(prixTotalPanier);
         
 
+
 }else{
 document.getElementById('panierVide').innerText = "Votre panier est vide";
 console.log('hello');
  }
 
- //*************************** Gestion du boutton supprimer l'article *************************/
-  
+ //*************************** Gestion du boutton supprimer l'article ************************* 
+ 
+ 
 
 
-// sélection de l'id du produit qui va être supprimer en cliquant sur le boutton supprimer 
 
 
-let btnSupprimer = document.querySelectorAll(".btn-supprimer");
-console.log(btnSupprimer);
 
-//Suppression de l'élément  "produit supprimer"
+ //*************************** Fin DE la Gestion du boutton supprimer l'article *************************  
 
-    btnSupprimer.forEach((element) => {
 
-    element.addEventListener("click" , (event) =>{ 
-    event.preventDefault()
-    
-    let idProduitSupprimer = element.id;
-    let colorProduitSupprimer = element.color;
-    console.log('id') 
-    console.log(idProduitSupprimer) 
-    console.log('color')
-    console.log(colorProduitSupprimer)
-
-// avec la methode filter je séléctionne  les elements à garder et je supprime l'élément où le btn supprimer a été cliqué
-
-    let newProduitEnregistrer = produitEnregistreDansLocalStorage.filter(ele => ele.id !== idProduitSupprimer  && ele.color !== colorProduitSupprimer)
-    console.log('newProduitEnregistrer');
-    console.log(newProduitEnregistrer);
-    // on envoie la variable "newProduitEnregistrer" dans le local storage
-    // la transformation en format JSON et l'envoyer dans la key "basket" du local storage
-    localStorage.setItem("basket", JSON.stringify(newProduitEnregistrer));
-    });
-    
-})
-//**************************************** */ Fin de Suppression de l'élément  "produit supprimer"*******************
 
 // ***************************************addEventListener pour le boutton "je valide mon panier et mes informations" formulaire***********************
 const btnEnvoyerFormulaire = document.getElementById("envoiFormulaire");
-btnEnvoyerFormulaire.addEventListener("click", (e)=>{
-e.preventDefault();
+btnEnvoyerFormulaire.addEventListener("click", ()=>{
+// e.preventDefault(); // a enlever
 
-    //*****************************************récuperation des valeurs du formulaire de commande pour le mettre dans le local storage*********************************************/
+    //*****************************************récuperation des valeurs du formulaire de commande pour les mettre dans le local storage*********************************************/
 
-var formulaireValues = {
-    prenom : document.getElementById("prenom").value,    
-    nom : document.getElementById("nomUtilisateur").value,
-    mail : document.getElementById("mail").value,
-    adresse : document.getElementById("adresse").value,
-    ville : document.getElementById("ville").value,
-    codePostal : document.getElementById("codePostal").value
-}
+const contact = {
+    firstName: document.getElementById("prenom").value,    
+    lastName : document.getElementById("nomUtilisateur").value,
+    email : document.getElementById("mail").value,
+    address : document.getElementById("adresse").value,
+    city : document.getElementById("ville").value,   
+};
 
-console.log("formulaireValues");
-console.log(formulaireValues);
+localStorage.setItem("contact", JSON.stringify(contact));
 
+const products = produitEnregistreDansLocalStorage.map(
+    (produit) => produit.id
+);
 
-// ***************************gestion validation de formulaire (contrôle du formulaire)
-
-var regExPrenomNomVille = (value) => {
-    return /^[A-Za-z]{3,20}$/.test(value) 
-}
-// fonction pour controler de la validité du prenom nom et ville
-function prenomNomVilleControle(){
-            
-        var lePrenom = formulaireValues.prenom;
-        var leNom = formulaireValues.nom;
-        var laVille = formulaireValues.ville;        
-
-        if(regExPrenomNomVille(lePrenom) && regExPrenomNomVille(leNom) && regExPrenomNomVille(laVille)){
-            return true;
-        }else{            
-            alert("chiffre et symbole non autorisé \n Ne pas dépasser 20 caractères, minmum 3 caractères");
-            return false;
-        };
-}
-// fonction pour controler  la validité du code postal
-
-function codePostalControle(){
-            
-    var leCodepostal = formulaireValues.codePostal;          
-
-    if(/^[0-9]{5}$/.test (leCodepostal)){
-        return true;
-    }else{            
-        alert("Code Postal : doit être composé de 5 chiffres");
-        return false;
-    };
-}
-
-// fonction pour controler  la validité du email
-
-function mailControle(){
-            
-    var leMail = formulaireValues.mail;          
-
-    if(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test (leMail)){
-        return true;
-    }else{            
-        alert("Adresse e-mail non valid");
-        return false;
-    };
-}
-
-if(prenomNomVilleControle() && codePostalControle() && mailControle() ){
-// envoyer les valeurs du formulaire de commande dans le local storage s'il est rempli correctement
-localStorage.setItem("formulaireValues", JSON.stringify(formulaireValues));
-
-}else{
-    alert("Veuillez bien remplir le formulaire");
-}
+console.log("products");
+console.log(products);
+console.log("contact");
+console.log(contact);
 
 
-
-// *******Fin gestion validation de formulaire (contrôle du formulaire)
-
-//*****************************************Fin récuperation des valeurs du formulaire de commande pour le mettre dans le local storage********
+//*****************************************Fin récuperation des valeurs du formulaire de commande pour les mettre dans le local storage********
 
 
-// mettre les values du formulaire et les produits selectionnées dans l'objet 'aEnvoyer'  à envoyer vers le serveur
+// mettre les values du formulaire et les produits selectionnées dans l'objet 'aEnvoyer'  pour les  envoyer vers le serveur
 
+/*
 var aEnvoyer = { 
-    produitEnregistreDansLocalStorage,
-    formulaireValues
+   products,
+   contact
 }
 
 console.log('aEnvoyer');
 console.log(aEnvoyer);
+*/
 
 //  Envoie de l'objet 'aEnvoyer' vers le serveur
-var promise01 = fetch("http://jsonplaceholder.typicode.com/users", {
+
+    fetch("http://localhost:3000/api/teddies/order", {
     method: "POST",
-    body: JSON.stringify(aEnvoyer),
+    body: JSON.stringify({ 
+        products,
+        contact
+     }),
     headers: {
-        "Content-type" : "application:json",
+        Accept: "application/json",
+        "Content-Type": "application/json",
     },
 });
-console.log("promise01");
-console.log(promise01);
+
 
 });
 
-// ************************mettre le contenu de la key 'formulaireValues' du local storage dans les champs du formulaire 
-// prendre  la key 'formulaireValues' du local storage est la mettre dans une variable
+// ************************mettre le contenu de la key 'contact' du local storage dans les champs du formulaire 
+// prendre  la key 'contact' du local storage est la mettre dans une variable
 
-var formulaireValuesLocalStorage = JSON.parse(localStorage.getItem("formulaireValues"));
+
+var formulaireValuesLocalStorage = JSON.parse(localStorage.getItem("contact")) || [];
 console.log("formulaireValuesLocalStorage");
 console.log(formulaireValuesLocalStorage);
 // mettre les valeurs "formulaireValuesLocalStorage" dans les champs du formulaire
-document.getElementById("prenom").value = formulaireValuesLocalStorage.prenom;
-document.getElementById("nomUtilisateur").value = formulaireValuesLocalStorage.nom;
-document.getElementById("mail").value = formulaireValuesLocalStorage.mail;
-document.getElementById("adresse").value = formulaireValuesLocalStorage.adresse;
-document.getElementById("ville").value = formulaireValuesLocalStorage.ville;
-document.getElementById("codePostal").value = formulaireValuesLocalStorage.codePostal;
+document.getElementById("prenom").value = formulaireValuesLocalStorage.firstName;
+document.getElementById("nomUtilisateur").value = formulaireValuesLocalStorage.lastName;
+document.getElementById("mail").value = formulaireValuesLocalStorage.email;
+document.getElementById("adresse").value = formulaireValuesLocalStorage.address;
+document.getElementById("ville").value = formulaireValuesLocalStorage.city;
+// document.getElementById("codePostal").value = formulaireValuesLocalStorage.codePostal;
 
-// ************************ fin mettre le contenu de la key 'formulaireValues' du local storage dans les champs du formulaire 
+// ************************ fin mettre le contenu de la key 'contact' du local storage dans les champs du formulaire 
